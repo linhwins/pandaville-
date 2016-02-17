c275f2a30a95f85b18abe70f7541ca87899d9dfa
Linh Nguyen <babyxox91@yahoo.com>
2016-02-17 18:00:26 -0500

    Background/Title Change

--- js/main.js
+++ js/main.js
@@ -17,27 +17,30 @@ window.onload = function() {
     
     function preload() {
         // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+        game.load.image( 'logo', 'assets/bamboo1.jpg' );
+        game.load.spritesheet('dude', 'assets/pandasprites2.png', 24, 34);
     }
     
     var bouncy;
+    var s; 
     
     function create() {
         // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
+        /////bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
+        s = game.add.tileSprite(0, 0, 2000, 2000, 'logo');
         // Anchor the sprite at its center, as opposed to its top-left corner.
         // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
+        ////bouncy.anchor.setTo( 0.5, 0.5 );
         
         // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
+        ////////game.physics.enable( bouncy, Phaser.Physics.ARCADE );
         // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
+        ////////////bouncy.body.collideWorldBounds = true;
         
         // Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something awesome.", style );
+        var style = { font: "60px Broadway", fill: "#190718", align: "center" };
+        var text = game.add.text( game.world.centerX, 15, "PANDAVILLE", style );
         text.anchor.setTo( 0.5, 0.0 );
     }
     
@@ -47,6 +50,6 @@ window.onload = function() {
         // in X or Y.
         // This function returns the rotation angle that makes it visually match its
         // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
+        //////bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
     }
 };
